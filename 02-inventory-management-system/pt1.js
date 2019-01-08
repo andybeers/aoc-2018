78769ea99e2175c3b56fe31c4d412d55b1077aeb
Andrew Beers <andybeers@gmail.com>
2019-01-07 21:46:39 -0800

    added andydata to pt1, ran prettier

--- 02-inventory-management-system/pt1.js
+++ 02-inventory-management-system/pt1.js
@@ -4,46 +4,33 @@ const data = fs.readFileSync('taraData.txt', 'utf8')
 
 const dataArray = data.split('\n')
 
-// abcdef contains no letters that appear exactly two or three times.
-// bababc contains two a and three b, so it counts for both.
-// abbcde contains two b, but no letter appears exactly three times.
-// abcccd contains three c, but no letter appears exactly two times.
-// aabcdd contains two a and two d, but it only counts once.
-// abcdee contains two e.
-// ababab contains three a and three b, but it only counts once.
-
-let twoCounter = 0;
-let threeCounter = 0;
+let twoCounter = 0
+let threeCounter = 0
 
 function searchString(str) {
-  const letters = str.split("");
+  const letters = str.split('')
 
   const frequencyMap = letters.reduce((acc, letter) => {
-    acc[letter] = acc[letter] ? acc[letter]+1 : 1;
-    return acc;
-  }, {});
+    acc[letter] = acc[letter] ? acc[letter] + 1 : 1
+    return acc
+  }, {})
 
-  const frequencyArr = Object.keys(frequencyMap);
+  const frequencyArr = Object.keys(frequencyMap)
+  const hasTwo = !!frequencyArr.find(letter => frequencyMap[letter] === 2)
+  const hasThree = !!frequencyArr.find(letter => frequencyMap[letter] === 3)
 
-  const hasTwo = !!frequencyArr.find((letter) => {
-    return frequencyMap[letter] === 2;
-  });
-
-  const hasThree = !!frequencyArr.find((letter) => {
-    return frequencyMap[letter] === 3;
-  });
-  
-  if(hasTwo){twoCounter++};
-  if(hasThree){threeCounter++};
+  if (hasTwo) {
+    twoCounter++
+  }
+  if (hasThree) {
+    threeCounter++
+  }
 }
 
-dataArray.forEach((str) => {
-  searchString(str);
+dataArray.forEach(str => {
+  searchString(str)
 })
 
-console.log(twoCounter);
-console.log(threeCounter);
+const checksum = twoCounter * threeCounter
 
-const checksum = twoCounter * threeCounter;
-
-console.log({ checksum });
+console.log({ checksum })
