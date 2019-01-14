873ea152360c1b8ff8f7710d7e3e8e5271453cef
Tara <tara.kreft@gmail.com>
2019-01-13 21:24:35 -0800

    the best algorithms

--- 03-no-matter-how-you-slice-it/pt1.js
+++ 03-no-matter-how-you-slice-it/pt1.js
@@ -1,5 +1,52 @@
 const readFile = require('../utils/read-data-file')
-
 const data = readFile(process.argv[2])
+const dataArray = data.split('\n')
+// const dataArray = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
 
-console.log(data)
+const decoratedData = dataArray.map(row => {
+  const xCoord = parseInt(row.match('\@ ([0123456789]+)')[1], 10)
+  const yCoord = parseInt(row.match('\,([0123456789]+)')[1], 10)
+  const width = parseInt(row.match('\: ([0123456789]+)')[1], 10)
+  const height = parseInt(row.match('x([0123456789]+)')[1], 10)
+
+  return {
+    xCoord, yCoord, width, height
+  }
+})
+
+function buildFrequencyMap(data) {
+  return data.reduce((acc, curr) => {
+
+    for (let i = 0; i < curr.height; i++) {     
+      let y = curr.yCoord + i
+  
+      for (let j = 0; j < curr.width; j++) {     
+        let x = curr.xCoord + j
+
+        let position = acc[`${x},${y}`]
+        acc[`${x},${y}`] = position ? position += 1 : 1
+      }
+    }
+
+    return acc
+  }, {})
+}
+
+const frequencyMap = buildFrequencyMap(decoratedData)
+
+const sharedTileCount = Object.keys(frequencyMap).reduce((acc, curr) => {
+  if (frequencyMap[curr] > 1) {
+    acc = ++acc
+  }
+  return acc
+}, 0)
+
+console.log(sharedTileCount)
+
+
+// #1 @ 1,3: 4x4
+
+// 13 23 33 43
+// 14 24 34 44
+// 15 25 35 45
+// 16 26 36 37
